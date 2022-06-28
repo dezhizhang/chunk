1df4be4ddb9e522d760efc44c5514883159e00e9
dezhizhang <1541609448@qq.com>
2022-06-29 06:50:39 +0800

    feat:提取公共代码

--- plugins/preload-webpack-plugin.js
+++ plugins/preload-webpack-plugin.js
@@ -27,7 +27,28 @@ class PreloadWebpackPlugin {
       );
     });
   }
-  generateLinks(compilation, htmlData, callback) {}
+  generateLinks(compilation, htmlData, callback) {
+    //1 获取所有代码块
+    const chunks = [...compilation.chunks];
+    //获取导步代码块
+    const asycnChunks = chunks.filter(chunk => chunk.canBeInitial());
+    const allFiles = asycnChunks.reduce((accumulated,chunk) => {
+        return accumulated.add(...chunk.files)
+    },new Set());
+
+    const links = [];
+    for(const file of allFiles.values()) {
+        links.push({
+            tagName:'link',
+            attributes:{
+                rol:'preload',
+                href:file
+            }
+        })
+    }
+    this.resourceHints = links;
+    callback();
+  }
 }
 
 module.exports = PreloadWebpackPlugin;
